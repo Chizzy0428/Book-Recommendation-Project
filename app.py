e2fc76ead4dff28d47cda4379f22d015c9efe412
Nwabuisi Chizaram F <137488570+Chizzy0428@users.noreply.github.com>
2025-01-01 11:28:42 +0000

    update

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 import numpy as np
 
 st.header('Book Recommender System Using Machine learning')
+model = pickle.load(open('artifacts/model.pkl', 'rb'))
+books_name = pickle.load(open('artifacts/book_name.pkl', 'rb'))
+final_rating = pickle.load(open('artifacts/final_rating.pkl', 'rb'))
+books_pivot = pickle.load(open('artifacts/book_pivot.pkl', 'rb'))
